25c1294135ceae41a0d69462944a54e67d45c54c
Isaura Fontcuberta <isafontcu@gmail.com>
2019-11-20 19:08:32 +0100

    Adding Open Sans fonts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react"
 import Navigator from "../components/Navigator"
 import Head from "next/head"
-import { CssBaseline, Container, TextField } from "@material-ui/core"
+import {
+  CssBaseline,
+  Container,
+  TextField,
+  createMuiTheme,
+  MuiThemeProvider,
+} from "@material-ui/core"
 import {
   InputLabel,
   MenuItem,
@@ -16,6 +22,16 @@ import { CompanyType, MeasureGoal, OnboardingType } from "../domain/CalculateTot
 
 const bindEventValueTo = (eventHandelerFn: any) => (event: any) =>
   eventHandelerFn(event.target.value)
+
+const THEME = createMuiTheme({
+  typography: {
+    fontFamily: '"Open Sans", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+})
 
 const Home = () => {
   const [customerName, setCustomerName] = useState("")
@@ -59,218 +75,222 @@ const Home = () => {
 
   return (
     <div>
-      <Head>
-        <title>Cuantix Calculator</title>
-        <link rel="icon" href="favicon.ico" />
-      </Head>
-      <React.Fragment>
-        <Navigator />
-        <CssBaseline />
-        <Container maxWidth="md">
-          <div className="hero">
-            <h1 className="title">Cuantix Calculator</h1>
-            <h2>
-              ¿Quieres saber cuánto podría costar tu medición? Contesta algunas preguntas y te
-              enviaremos un presupuesto estimado.
-            </h2>
-            <form noValidate autoComplete="off">
-              <h3>Háblanos un poco de tu empresa</h3>
-              <div className="selector">
-                <InputLabel>Tu empresa es...</InputLabel>
-                <FormControl>
-                  <Select
-                    id="companyType"
-                    value={companyType}
-                    onChange={bindEventValueTo(setCompanyType)}
-                  >
-                    <MenuItem value={CompanyType.MULTINATIONAL_CORPORATION}>
-                      Multinacional / Corporación
-                    </MenuItem>
-                    <MenuItem value={CompanyType.COMPANY}>Empresa</MenuItem>
-                    <MenuItem value={CompanyType.NGO}>NGO</MenuItem>
-                  </Select>
-                  <FormHelperText>
-                    Selecciona la opción que mejor describe a tu empresa
-                  </FormHelperText>
-                </FormControl>
-              </div>
+      <MuiThemeProvider theme={THEME}>
+        <Head>
+          <title>Cuantix Calculator</title>
+          <link rel="icon" href="favicon.ico" />
+        </Head>
+        <React.Fragment>
+          <Navigator />
+          <CssBaseline />
+          <Container maxWidth="md">
+            <div className="hero">
+              <h1 className="title">Cuantix Calculator</h1>
+              <h2>
+                ¿Quieres saber cuánto podría costar tu medición? Contesta algunas preguntas y te
+                enviaremos un presupuesto estimado.
+              </h2>
+              <form noValidate autoComplete="off">
+                <h3>Háblanos un poco de tu empresa</h3>
+                <div className="selector">
+                  <InputLabel>Tu empresa es...</InputLabel>
+                  <FormControl>
+                    <Select
+                      id="companyType"
+                      value={companyType}
+                      onChange={bindEventValueTo(setCompanyType)}
+                    >
+                      <MenuItem value={CompanyType.MULTINATIONAL_CORPORATION}>
+                        Multinacional / Corporación
+                      </MenuItem>
+                      <MenuItem value={CompanyType.COMPANY}>Empresa</MenuItem>
+                      <MenuItem value={CompanyType.NGO}>NGO</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      Selecciona la opción que mejor describe a tu empresa
+                    </FormHelperText>
+                  </FormControl>
+                </div>
 
-              <h3>Cuéntanos sobre tu medición</h3>
-              <div className="selector">
-                <InputLabel>¿Cuál es el objetivo de la medición?</InputLabel>
-                <FormControl>
-                  <Select
-                    id="measureGoal"
-                    value={measureGoal}
-                    onChange={bindEventValueTo(setMeasureGoal)}
-                  >
-                    <MenuItem value={MeasureGoal.PUBLIC_POLICY}>
-                      Influir en políticas públicas
-                    </MenuItem>
-                    <MenuItem value={MeasureGoal.INVESTMENT_DECISION}>
-                      Tomar decisiones de inversión
-                    </MenuItem>
-                    <MenuItem value={MeasureGoal.FUNDRAISING}>Levantar fondos</MenuItem>
-                    <MenuItem value={MeasureGoal.REVENUE_ASSURANCE}>Demostrar el retorno</MenuItem>
-                    <MenuItem value={MeasureGoal.ACCOUNTABILITY}>Rendición de cuentas</MenuItem>
-                    <MenuItem value={MeasureGoal.INSIGHTS_FINDING}>
-                      Conocer insights o necesidades
-                    </MenuItem>
-                  </Select>
-                  <FormHelperText>
-                    Escoge el objetivo principal por el que quieres medir
-                  </FormHelperText>
-                </FormControl>
-              </div>
-              <div className="selector">
-                <InputLabel>
-                  ¿Cuántas organizaciones intervienen en la creación y captura de datos del
-                  proyecto?
-                </InputLabel>
+                <h3>Cuéntanos sobre tu medición</h3>
+                <div className="selector">
+                  <InputLabel>¿Cuál es el objetivo de la medición?</InputLabel>
+                  <FormControl>
+                    <Select
+                      id="measureGoal"
+                      value={measureGoal}
+                      onChange={bindEventValueTo(setMeasureGoal)}
+                    >
+                      <MenuItem value={MeasureGoal.PUBLIC_POLICY}>
+                        Influir en políticas públicas
+                      </MenuItem>
+                      <MenuItem value={MeasureGoal.INVESTMENT_DECISION}>
+                        Tomar decisiones de inversión
+                      </MenuItem>
+                      <MenuItem value={MeasureGoal.FUNDRAISING}>Levantar fondos</MenuItem>
+                      <MenuItem value={MeasureGoal.REVENUE_ASSURANCE}>
+                        Demostrar el retorno
+                      </MenuItem>
+                      <MenuItem value={MeasureGoal.ACCOUNTABILITY}>Rendición de cuentas</MenuItem>
+                      <MenuItem value={MeasureGoal.INSIGHTS_FINDING}>
+                        Conocer insights o necesidades
+                      </MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      Escoge el objetivo principal por el que quieres medir
+                    </FormHelperText>
+                  </FormControl>
+                </div>
+                <div className="selector">
+                  <InputLabel>
+                    ¿Cuántas organizaciones intervienen en la creación y captura de datos del
+                    proyecto?
+                  </InputLabel>
+                  <TextField
+                    id="numberOfOrganizations"
+                    label="Organizaciones"
+                    type="number"
+                    InputProps={{ inputProps: { min: 1, max: 20 } }}
+                    value={numberOfOrganizations}
+                    onChange={event => {
+                      const number = parseInt(event.target.value)
+                      setNumberOfOrganizations(number)
+                    }}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    margin="normal"
+                  />
+                </div>
+                <Typography id="slider" gutterBottom>
+                  ¿Cuántos proyectos quieres medir?
+                </Typography>
+                <Slider
+                  value={numberOfProjects}
+                  defaultValue={1}
+                  step={1}
+                  min={1}
+                  max={10}
+                  marks={marks}
+                  valueLabelDisplay="on"
+                  onChange={(_event, newValue) => {
+                    if (!Array.isArray(newValue)) {
+                      setNumberOfProjects(newValue)
+                    }
+                  }}
+                />
+                <div className="selector">
+                  <InputLabel>¿Cómo quieres hacer el onboarding?</InputLabel>
+                  <FormControl>
+                    <Select
+                      id="onboardingType"
+                      value={onboardingType}
+                      onChange={bindEventValueTo(setOnboardingType)}
+                    >
+                      <MenuItem value={OnboardingType.NO_ONBOARDING}>Sin Onboarding</MenuItem>
+                      <MenuItem value={OnboardingType.THEORY_OF_CHANGE}>
+                        Diseño Teoría de Cambio
+                      </MenuItem>
+                      <MenuItem value={OnboardingType.GROUP_WEBINAR}>Webinar Grupal</MenuItem>
+                      <MenuItem value={OnboardingType.INDIVIDUAL_WEBINAR}>
+                        Webinar Individual
+                      </MenuItem>
+                      <MenuItem value={OnboardingType.CUSTOMIZED}>Personalizado</MenuItem>
+                    </Select>
+                    <FormHelperText>
+                      El proceso de onboarding con nuestros consultores te ayudara a conseguir la
+                      mejor medición para tu proyecto
+                    </FormHelperText>
+                  </FormControl>
+                </div>
+
+                <h3>Finalmente, ¿cómo te contactamos?</h3>
                 <TextField
-                  id="numberOfOrganizations"
-                  label="Organizaciones"
-                  type="number"
-                  InputProps={{ inputProps: { min: 1, max: 20 } }}
-                  value={numberOfOrganizations}
-                  onChange={event => {
-                    const number = parseInt(event.target.value)
-                    setNumberOfOrganizations(number)
-                  }}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
+                  id="customerName"
+                  label="Nombre"
+                  value={customerName}
+                  onChange={bindEventValueTo(setCustomerName)}
+                  fullWidth
+                  required
+                  autoFocus
                   margin="normal"
                 />
-              </div>
-              <Typography id="slider" gutterBottom>
-                ¿Cuántos proyectos quieres medir?
-              </Typography>
-              <Slider
-                value={numberOfProjects}
-                defaultValue={1}
-                step={1}
-                min={1}
-                max={10}
-                marks={marks}
-                valueLabelDisplay="on"
-                onChange={(_event, newValue) => {
-                  if (!Array.isArray(newValue)) {
-                    setNumberOfProjects(newValue)
-                  }
-                }}
-              />
-              <div className="selector">
-                <InputLabel>¿Cómo quieres hacer el onboarding?</InputLabel>
-                <FormControl>
-                  <Select
-                    id="onboardingType"
-                    value={onboardingType}
-                    onChange={bindEventValueTo(setOnboardingType)}
-                  >
-                    <MenuItem value={OnboardingType.NO_ONBOARDING}>Sin Onboarding</MenuItem>
-                    <MenuItem value={OnboardingType.THEORY_OF_CHANGE}>
-                      Diseño Teoría de Cambio
-                    </MenuItem>
-                    <MenuItem value={OnboardingType.GROUP_WEBINAR}>Webinar Grupal</MenuItem>
-                    <MenuItem value={OnboardingType.INDIVIDUAL_WEBINAR}>
-                      Webinar Individual
-                    </MenuItem>
-                    <MenuItem value={OnboardingType.CUSTOMIZED}>Personalizado</MenuItem>
-                  </Select>
-                  <FormHelperText>
-                    El proceso de onboarding con nuestros consultores te ayudara a conseguir la
-                    mejor medición para tu proyecto
-                  </FormHelperText>
-                </FormControl>
-              </div>
+                <TextField
+                  id="customerLastname"
+                  label="Apellido"
+                  value={customerLastname}
+                  onChange={bindEventValueTo(setCustomerLastname)}
+                  fullWidth
+                  required
+                  margin="normal"
+                />
+                <TextField
+                  id="customerEmail"
+                  label="Email"
+                  value={customerEmail}
+                  onChange={bindEventValueTo(setCustomerEmail)}
+                  fullWidth
+                  required
+                  margin="normal"
+                />
+                <TextField
+                  id="customerPhone"
+                  label="Número de Teléfono"
+                  value={customerPhone}
+                  onChange={bindEventValueTo(setCustomerPhone)}
+                  fullWidth
+                  required
+                  margin="normal"
+                />
+                <TextField
+                  id="customerCountry"
+                  label="País"
+                  value={customerCountry}
+                  onChange={bindEventValueTo(setCustomerCountry)}
+                  fullWidth
+                  margin="normal"
+                />
 
-              <h3>Finalmente, ¿cómo te contactamos?</h3>
-              <TextField
-                id="customerName"
-                label="Nombre"
-                value={customerName}
-                onChange={bindEventValueTo(setCustomerName)}
-                fullWidth
-                required
-                autoFocus
-                margin="normal"
-              />
-              <TextField
-                id="customerLastname"
-                label="Apellido"
-                value={customerLastname}
-                onChange={bindEventValueTo(setCustomerLastname)}
-                fullWidth
-                required
-                margin="normal"
-              />
-              <TextField
-                id="customerEmail"
-                label="Email"
-                value={customerEmail}
-                onChange={bindEventValueTo(setCustomerEmail)}
-                fullWidth
-                required
-                margin="normal"
-              />
-              <TextField
-                id="customerPhone"
-                label="Número de Teléfono"
-                value={customerPhone}
-                onChange={bindEventValueTo(setCustomerPhone)}
-                fullWidth
-                required
-                margin="normal"
-              />
-              <TextField
-                id="customerCountry"
-                label="País"
-                value={customerCountry}
-                onChange={bindEventValueTo(setCustomerCountry)}
-                fullWidth
-                margin="normal"
-              />
+                <Button className="button" onClick={handleSubmit}>
+                  SUBMIT
+                </Button>
+              </form>
+            </div>
+          </Container>
+        </React.Fragment>
 
-              <Button className="button" onClick={handleSubmit}>
-                SUBMIT
-              </Button>
-            </form>
-          </div>
-        </Container>
-      </React.Fragment>
-
-      <style jsx>{`
-        .button {
-          width: 100% !important;
-          color: red !important;
-          border: 1px solid #9b9b9b;
-          background-color: #230000;
-        }
-        .hero {
-          width: 100%;
-          color: #333;
-        }
-        .title {
-          margin: 0;
-          width: 100%;
-          padding-top: 80px;
-          line-height: 1.15;
-          font-size: 48px;
-          color: #696969;
-        }
-        .title,
-        .description {
-          text-align: center;
-        }
-        .selector {
-          margin-top: 20px;
-          width: 100%;
-        }
-        #slider {
-          margin-top: 210px;
-        }
-      `}</style>
+        <style jsx>{`
+          .button {
+            width: 100% !important;
+            color: red !important;
+            border: 1px solid #9b9b9b;
+            background-color: #230000;
+          }
+          .hero {
+            width: 100%;
+            color: #333;
+          }
+          .title {
+            margin: 0;
+            width: 100%;
+            padding-top: 80px;
+            line-height: 1.15;
+            font-size: 48px;
+            color: #696969;
+          }
+          .title,
+          .description {
+            text-align: center;
+          }
+          .selector {
+            margin-top: 20px;
+            width: 100%;
+          }
+          #slider {
+            margin-top: 210px;
+          }
+        `}</style>
+      </MuiThemeProvider>
     </div>
   )
 }
